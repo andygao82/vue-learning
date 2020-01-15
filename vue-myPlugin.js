7c34d257a4a14d098275e90b0fccc052f74a32d3
andygao82 <freedomandy82@gmail.com>
2020-01-15 22:31:43 +1100

    cli

---
+++ vue-myPlugin.js
@@ -0,0 +1,26 @@
+<html>
+<body>
+<head>
+    <script src="https://cdn.jsdelivr.net/npm/vue/dist/vue.js"></script>
+</head>
+<style>
+
+</style>
+<div id="app">
+
+
+</div>
+<script>
+    const vm = new Vue({
+        el: '#app',
+        data:{
+
+        },
+        methods:{
+
+        }
+
+    })
+</script>
+</body>
+</html>
